5e9c50f45a40e859f8e7e1f39791f82d3a820cca
Opjasson <azmitheusos333@gmail.com>
2025-09-19 02:24:48 +0700

    v-band : multiple attributes example

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -2,15 +2,30 @@
     <div class="hello">
         <h1>hallo vue</h1>
     </div>
-    <h1>{{ nama }}</h1>
+    <h1 :class="hello">Rudi</h1>
+
+
     <h1 v-html="nama"></h1>
     <h1 :id v-bind:class="nama">{{ nama }}</h1>
+
+    <button :disabled="checkit">Press true</button>
+
+    <h2 v-bind="paragraf">Saya sedang mencari kehidupan</h2>
 </template>
 
 <script setup>
 console.log("Hello Vue");
-const nama = "<b> azmi </b>"
-const id = "testing"
+const nama = "<b> azmi </b>";
+const id = "testing";
+const hello = "hello";
+
+// multiple attributes
+const paragraf = {
+    class : "hello",
+    id : "hello"
+}
+
+const checkit = true;
 </script>
 
 <style scoped>
