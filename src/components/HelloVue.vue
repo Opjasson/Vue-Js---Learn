46bb2a174311e09dd9eb69ba7bce234c44eac358
Opjasson <azmitheusos333@gmail.com>
2025-09-18 15:39:57 +0700

    feat(vue and js file) : create new file

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div class="hello">
+        <h1>hallo vue</h1>
+    </div>
+</template>
+
+<script setup>
+console.log("Hello Vue");
+</script>
+
+<style lang="" scoped>
+.hello {
+    color: green;
+}
+</style>
