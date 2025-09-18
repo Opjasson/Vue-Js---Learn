313445d56e24511fdcbe567e56983f985b75f2a1
Opjasson <azmitheusos333@gmail.com>
2025-09-18 23:12:59 +0700

    vue : v-html testing

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -2,10 +2,13 @@
     <div class="hello">
         <h1>hallo vue</h1>
     </div>
+    <h1>{{ nama }}</h1>
+    <h1 v-html="nama"></h1>
 </template>
 
 <script setup>
 console.log("Hello Vue");
+const nama = "<b> azmi </b>"
 </script>
 
 <style scoped>
