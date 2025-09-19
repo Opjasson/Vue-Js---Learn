d58af21ab601f7665e096d331848a5e295fd9c81
Opjasson <azmitheusos333@gmail.com>
2025-09-19 13:26:49 +0700

    Object reactive state : create object state to change value count

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -8,6 +8,12 @@
         <br />
         <!-- ------ -->
         <button v-on:click="incrementState">Add Counter state</button>
+
+        <!-- Complex --------- -->
+        <h1>Nama Pegawai : {{ objectEmploye.nama }}</h1>
+        <h1>Bayaran : {{ objectEmploye.count }}</h1>
+
+        <button v-on:click="incrementComplex">Tambah Bayaran</button>
     </div>
 </template>
 
@@ -16,6 +22,12 @@ import { ref } from "vue";
 
 let counter = 0;
 let stateCount = ref(0);
+
+// Complex state Object
+let objectEmploye = ref({
+    nama: "budi",
+    count: 1000,
+});
 
 // Counter using DOM
 function increment() {
@@ -26,6 +38,14 @@ function increment() {
 // Counter using reactive state
 function incrementState() {
     stateCount.value++;
+}
+
+// Complex object Reactive state
+function incrementComplex() {
+    objectEmploye.value = {
+      nama : objectEmploye.value.nama,
+      count : objectEmploye.value.count + 1000
+    }
 }
 </script>
 
